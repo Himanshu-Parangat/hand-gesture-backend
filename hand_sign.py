08236ebb114d1ab9f6df5357d3b20783374a513a
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-03-20 18:35:08 +0530

    fix typo

--- hand_sign.py
+++ hand_sign.py
@@ -1,26 +1,31 @@
 from typing import Any
 import cv2
 
-def inatialize_camera(cameraImdex:int = 0) -> cv2.VideoCapture:
-    cam = cv2.VideoCapture(cameraImdex)
+
+def initialize_camera(cameraIndex:int = 0) -> cv2.VideoCapture:
+    cam = cv2.VideoCapture(cameraIndex)
     cam.set(cv2.CAP_PROP_FPS, 60)
     return cam
 
+
 def capture_camera(cam_state: bool) -> Any:
-    cam = inatialize_camera()
+    cam = initialize_camera()
     while cam_state:
         _ , img = cam.read() 
         conv_img = convert_img(img,'rgb')
         cv2.imshow("Image", conv_img)
         cv2.waitKey(1)
 
-def convert_img(img: Any, format: str ) -> Any:
-    if format.lower() == 'bgr':
+
+def convert_img(img: Any, img_format: str ) -> Any:
+    if img_format.lower() == 'bgr':
         return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    elif format.lower() == 'rgb':
+    elif img_format.lower() == 'rgb':
         return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     else:
-        raise ValueError("Unsupported format. Please choose either 'bgr' or 'rgb'.")
+        raise ValueError("Unsupported img_format. expect either 'bgr' or 'rgb' ")
+
+
 
 def main() -> None:
     capture_camera(True)
