b5adeb0c9085d80f9e0e5b24372c5c92353d650f
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-03-21 00:44:33 +0530

    fix camera lag

--- hand_sign.py
+++ hand_sign.py
@@ -1,19 +1,26 @@
 from typing import Any
 import cv2
+import mediapipe  
 
 
-def initialize_camera(cameraIndex:int = 0) -> cv2.VideoCapture:
-    cam = cv2.VideoCapture(cameraIndex)
-    cam.set(cv2.CAP_PROP_FPS, 60)
+def initialize_camera(camera_index:int = 0) -> cv2.VideoCapture:
+    cam = cv2.VideoCapture(camera_index)
+    # cam.set(cv2.CAP_PROP_FPS, 60)
     return cam
 
 
 def capture_camera(cam_state: bool) -> Any:
     cam = initialize_camera()
+    hands = initialize_mediapipe()
+
     while cam_state:
         _ , img = cam.read() 
-        conv_img = convert_img(img,'rgb')
-        cv2.imshow("Image", conv_img)
+        img = cv2.flip(img,1)
+        rgb_img = convert_img(img,'rgb')
+
+        hands_landmarks = hands.process(rgb_img)
+        print(hands_landmarks.multi_hand_landmarks)
+        cv2.imshow("Image", img)
         cv2.waitKey(1)
 
 
@@ -25,6 +32,11 @@ def convert_img(img: Any, img_format: str ) -> Any:
     else:
         raise ValueError("Unsupported img_format. expect either 'bgr' or 'rgb' ")
 
+
+def initialize_mediapipe() -> Any:
+    mpHands = mediapipe.solutions.hands
+    hands = mpHands.Hands()
+    return hands
 
 
 def main() -> None:
