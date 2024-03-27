0f556d36cf1e2be491fad059d5c190a47df63f36
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-03-27 20:50:18 +0530

    rewrote in oops

--- hand_sign.py
+++ hand_sign.py
@@ -4,25 +4,7 @@ import mediapipe
 import numpy
 import time
 
-def initialize_camera(camera_index: int = 0) -> cv2.VideoCapture:
-    camera_feed = cv2.VideoCapture(camera_index)
-    return camera_feed
 
-
-
-def capture_frame(camera_feed: cv2.VideoCapture) -> numpy.ndarray:
-    inverted_frames = camera_feed.read()[1]
-    frames = cv2.flip(inverted_frames, 1)
-    return frames
-
-
-def convert_format(frames: Any, img_format: str) -> Any:
-    if img_format.lower() == 'bgr':
-        return cv2.cvtColor(frames, cv2.COLOR_RGB2BGR)
-    elif img_format.lower() == 'rgb':
-        return cv2.cvtColor(frames, cv2.COLOR_BGR2RGB)
-    else:
-        raise ValueError("Unsupported img_format. expect either 'bgr' or 'rgb' ")
 
 
 def initialize_mediapipe() -> mediapipe.solutions.hands.Hands:
@@ -58,21 +40,66 @@ def calculate_fps(previous_time: float) -> tuple[int, float]:
     return fps, current_time
 
 
-def main(process_cycle: bool) -> None:
-    camera_feed = initialize_camera()
-    mp_hands = initialize_mediapipe()
+# def main(process_cycle: bool) -> None:
+#     camera_feed = initialize_camera()
+#     mp_hands = initialize_mediapipe()
+#
+#     previous_time = time.time()
+#     while process_cycle:
+#         landmarked_frames, bgr_frames = mediapipe_landmark(camera_feed, mp_hands)
+#         connected_landmarked_frames = draw_hand_landmarks(bgr_frames, landmarked_frames)
+#
+#         fps, previous_time = calculate_fps(previous_time)
+#         print(f"fps : {fps}")
+#
+#         cv2.imshow("Image", connected_landmarked_frames)
+#         cv2.waitKey(1)
 
-    previous_time = time.time()
+def convert_format(frames: Any, img_format: str) -> Any:
+    if img_format.lower() == 'bgr':
+        return cv2.cvtColor(frames, cv2.COLOR_RGB2BGR)
+    elif img_format.lower() == 'rgb':
+        return cv2.cvtColor(frames, cv2.COLOR_BGR2RGB)
+    else:
+        raise ValueError("Unsupported img_format. expect either 'bgr' or 'rgb' ")
+
+
+class Camera:
+    def __init__(self):
+        self.camera_index = 0
+        self.camera_feed = cv2.VideoCapture(self.camera_index)
+
+    def capture_raw_fliped_frames(self) -> numpy.ndarray:
+        self.inverted_frames = self.camera_feed.read()[1]
+        return self.inverted_frames
+
+    def capture_frame(self) -> numpy.ndarray:
+        inverted_frames = self.capture_raw_fliped_frames()
+        frames = cv2.flip(inverted_frames, 1)
+        return frames
+
+
+def show_root_window(display_frames, window_name: str = "frames"):
+    cv2.imshow(window_name, display_frames)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        cv2.destroyAllWindows()
+        return False
+    return True
+
+
+def main():
+    process_cycle = True
+    webcam = Camera()
+    camera_feed = webcam.camera_feed
+
+
     while process_cycle:
-        landmarked_frames, bgr_frames = mediapipe_landmark(camera_feed, mp_hands)
-        connected_landmarked_frames = draw_hand_landmarks(bgr_frames, landmarked_frames)
+        # display_frames = webcam.capture_raw_fliped_frames()
+        display_frames = webcam.capture_frame()
 
-        fps, previous_time = calculate_fps(previous_time)
-        print(f"fps : {fps}")
-
-        cv2.imshow("Image", connected_landmarked_frames)
-        cv2.waitKey(1)
+        process_cycle = show_root_window(display_frames)
 
 
 if __name__ == "__main__":
-    main(True)
+    main()
