174e7e620ded998cfa57be3286bf861de7469ba3
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-05-01 11:11:51 +0530

    added specific field endpoint

--- endpoint_handlers.py
+++ endpoint_handlers.py
@@ -33,9 +33,40 @@ async def ping(data: dict = Body(...)):
 
 @app.get("/server/config/default")
 def get_default_config():
+    """
+    get the default stored config for the server
+    """
     return default_config
+
+
+@app.get("/server/config/default/{field}")
+def get_default_config_field(field: str):
+    """
+    from default config get specific field
+    """
+    field_entry = default_config.get(field, None)
+
+    if field_entry == None:
+        return f'{"message": "no field with name {field}"}'
+
+    return field_entry
 
 @app.get("/server/config/user")
 def get_user_config():
+    """
+    get the stored user config from the server
+    """
     return user_config
 
+
+@app.get("/server/config/user/{field}")
+def get_user_config_field(field: str):
+    """
+    from user config get specific field
+    """
+    field_entry = user_config.get(field, None)
+
+    if field_entry == None:
+        return f'{"message": "no field with name {field}"}'
+
+    return field_entry
