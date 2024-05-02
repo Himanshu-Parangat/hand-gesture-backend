022b529d4aa0c0d7e9014248a9f1068c25717cf7
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-05-02 13:28:43 +0530

    move models

--- endpoint_handlers.py
+++ endpoint_handlers.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI, Body, HTTPException
 from model_handlers import config_options
-from pydantic import BaseModel
 from model_handlers import default_config, user_config
-from enum import Enum
-
+from model_handlers import base_config, config_model
 
 
 app = FastAPI()
@@ -28,100 +26,12 @@ async def ping(data: dict = Body(...)):
     return {"message": "resetting the internal timer"}
 
 
-
-
-class HandTracking(BaseModel):
-    USE_STATIC_MODE: bool   
-    MAX_HANDS_COUNT: int  
-    MIN_DETECTION_THRESHOLD: float   
-    MIN_TRACKING_THRESHOLD: float  
-
-
-
-class FrameOrientation(str,Enum):
-    """possible value: "None" "clockwise" "180"  "counter-clockwise" """
-
-    NONE = "None"
-    CLOCKWISE = "clockwise"
-    COUNTER_CLOCKWISE = "counter_clockwise"
-    ZERO_DEGREE = "0"
-    ONE_EIGHTY_DEGREE = "180"
-
-
-class FrameFlip(str, Enum):
-    """possible value: "None" "horizontally" "vertically" "both" """
-
-    NONE = "None"
-    HORIZONTALLY = "horizontally"
-    VERTICALLY = "vertically"
-    BOTH = "both"
-
-class FrameFormat(str, Enum):
-    """possible value: "None" "BGR" "RGB" "HSV" "HLS" "Gray" """
-
-    BGR = "BGR"
-    RGB = "RGB"
-    HSV = "HSV"
-    HLS = "HLS"
-    GRAY = "GRAY"
-
-class Frames(BaseModel):
-    ORIENTATION: FrameOrientation 
-    FLIP_DIRECTION: FrameFlip 
-    FRAME_FORMAT: FrameFormat 
-
-class Camera_properties(BaseModel):
-    DEFAULT_CAMERA: int
-    FRAME_WIDTH: int
-    FRAME_HEIGHT: int
-    FPS: int
-    BRIGHTNESS: int
-    CONTRAST: int
-    SATURATION: int
-    HUE: int
-    GAIN: int
-    EXPOSURE: int
-
-
-class base_config(BaseModel):
-    handTracking: HandTracking
-    frames: Frames
-    camera_properties: Camera_properties 
-
-
-
-config = base_config.model_validate(default_config)
-
-
-@app.get("/server/config/default")
-def get_default_config() -> base_config: 
-    """
-    get the default stored config for the server
-    """
-    return config
-
-
-@app.post("/server/config/post")
+@app.post("/server/config/user")
 def post_default_config(config: base_config = Body(...)) -> base_config: 
     """
     get the default stored config for the server
     """
     return config
-
-
-@app.get("/server/config/default/{field}")
-def get_default_config_field(field: config_options) -> dict:
-    """
-    From default config get specific field
-    """
-
-    field_entry = field.value
-    field_value = default_config.get(field_entry)
-
-    if field_entry is None:
-        raise HTTPException(status_code=404, detail=f"no field with name {field}")
-    return {"entry": field_entry, "value": field_value}
-
 
 
 @app.get("/server/config/user")
@@ -140,6 +50,28 @@ def get_user_config_field(field: config_options) -> dict:
 
     field_entry = field.value
     field_value = user_config.get(field_entry)
+
+    if field_entry is None:
+        raise HTTPException(status_code=404, detail=f"no field with name {field}")
+    return {"entry": field_entry, "value": field_value}
+
+
+@app.get("/server/config/default")
+def get_default_config() -> base_config: 
+    """
+    get the default stored config for the server
+    """
+    return config_model
+
+
+@app.get("/server/config/default/{field}")
+def get_default_config_field(field: config_options) -> dict:
+    """
+    From default config get specific field
+    """
+
+    field_entry = field.value
+    field_value = default_config.get(field_entry)
 
     if field_entry is None:
         raise HTTPException(status_code=404, detail=f"no field with name {field}")
--- model_handlers.py
+++ model_handlers.py
@@ -1,5 +1,13 @@
 from enum import Enum
+from pydantic import BaseModel
 import json
+
+
+class ServerState(Enum):    
+    IDLE = "idle",
+    START = "start",
+    STOP = "stop",
+    TERMINATE = "terminate"
 
 
 class config_options(str,Enum):
@@ -8,12 +16,66 @@ class config_options(str,Enum):
     HANDTRACKING = "handTracking"
 
 
-class ServerState(Enum):    
-    IDLE = "idle",
-    START = "start",
-    STOP = "stop",
-    TERMINATE = "terminate"
-    
+class HandTracking(BaseModel):
+    USE_STATIC_MODE: bool   
+    MAX_HANDS_COUNT: int  
+    MIN_DETECTION_THRESHOLD: float   
+    MIN_TRACKING_THRESHOLD: float  
+
+
+class FrameOrientation(str,Enum):
+    """possible value: "None" "clockwise" "180"  "counter-clockwise" """
+
+    NONE = "None"
+    CLOCKWISE = "clockwise"
+    COUNTER_CLOCKWISE = "counter_clockwise"
+    ZERO_DEGREE = "0"
+    ONE_EIGHTY_DEGREE = "180"
+
+
+class FrameFlip(str, Enum):
+    """possible value: "None" "horizontally" "vertically" "both" """
+
+    NONE = "None"
+    HORIZONTALLY = "horizontally"
+    VERTICALLY = "vertically"
+    BOTH = "both"
+
+
+class FrameFormat(str, Enum):
+    """possible value: "None" "BGR" "RGB" "HSV" "HLS" "Gray" """
+
+    BGR = "BGR"
+    RGB = "RGB"
+    HSV = "HSV"
+    HLS = "HLS"
+    GRAY = "GRAY"
+
+
+class Frames(BaseModel):
+    ORIENTATION: FrameOrientation 
+    FLIP_DIRECTION: FrameFlip 
+    FRAME_FORMAT: FrameFormat 
+
+
+class Camera_properties(BaseModel):
+    DEFAULT_CAMERA: int
+    FRAME_WIDTH: int
+    FRAME_HEIGHT: int
+    FPS: int
+    BRIGHTNESS: int
+    CONTRAST: int
+    SATURATION: int
+    HUE: int
+    GAIN: int
+    EXPOSURE: int
+
+
+class base_config(BaseModel):
+    handTracking: HandTracking
+    frames: Frames
+    camera_properties: Camera_properties 
+
 
 with open('./config/default_config.json', 'r') as file:
     default_config = json.load(file)
@@ -22,6 +84,7 @@ with open('./config/user_config.json', 'r') as file:
     user_config = json.load(file)
 
 
+config_model = base_config.model_validate(default_config)
 
 if __name__ == "__main__":
    print(default_config) 
