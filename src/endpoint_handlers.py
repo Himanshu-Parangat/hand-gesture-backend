1059c1ce874ac875337fd02cc58e032203dc926f
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-05-09 23:59:16 +0530

    modefying structure

---
+++ src/endpoint_handlers.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI, Body, HTTPException
+from model_handlers import base_config, config_options
+from model_handlers import HandTracking, Frames, Camera_properties
+from model_handlers import default_model, user_model
+
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def status():
+    return {"status": "active"}
+
+
+@app.post("/ping")
+async def ping(data: dict = Body(...)):
+    """
+    Checks if client, is alive? and sending status at regular intervl
+
+    Raises a 422 Unprocessable Entity error if the request body is not a dictionary.
+    """
+
+    if data != {"status": "alive"}:
+        raise HTTPException(status_code=404, detail=f"expected format is  'status':'your status' ")
+
+    return {"message": "resetting the internal timer"}
+
+
+@app.post("/server/config/user")
+def post_default_config(config: base_config = Body(...)) -> base_config: 
+    """
+    get the default stored config for the server
+    """
+    return config
+
+
+@app.get("/server/config/user")
+def get_user_config() -> base_config:
+    """
+    get the stored user config from the server
+    """
+    return user_model
+
+
+@app.get("/server/config/user/{field}")
+def get_user_config_field(field: config_options) -> HandTracking|Frames|Camera_properties:
+    """
+    from user config get specific field
+    """
+    return getattr(user_model, field.value)
+
+
+@app.get("/server/config/default")
+def get_default_config() -> base_config: 
+    """
+    get the default stored config for the server
+    """
+    return default_model
+
+
+@app.get("/server/config/default/{field}")
+def get_default_config_field(field: config_options) -> HandTracking|Frames|Camera_properties:
+    """
+    From default config get specific field
+    """
+    return getattr(default_model, field.value)
---
+++ src/main.py
@@ -0,0 +1,84 @@
+import cv2
+import uvicorn
+from threading import Thread
+from config_handlers import config
+from camera_handlers import Camera
+from handtracker_handlers import HandTracker
+
+
+class ServerManager:
+    def __init__(self) -> None:
+        self.webcam = Camera()
+        self.handtracker = HandTracker()
+        self.server_state = True
+
+    def process_cycle(self, state):
+
+        while state:
+
+            frames, rgb_frames = self.webcam.capture_frame(config("orientation"), config("flip_direction"), config("frame_format"))
+            marks = self.handtracker.landmark(rgb_frames)
+
+            print(marks)
+            self.show_root_window(frames)
+
+            # Check for 'q' key press here
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                cv2.destroyAllWindows()
+                self.server_state = False
+                break  # Exit the loop
+
+    def show_root_window(self, display_frames, window_name: str = "frames"):
+        cv2.imshow(window_name, display_frames)
+
+
+class EndpointManager:
+    def __init__(self) -> None:
+        self.endpoint_state = True
+
+    def process_cycle(self, state):
+        while state:
+            uvicorn.run("endpoint_handlers:app", host="0.0.0.0", port=8000, reload=True)
+
+            break
+
+
+# class ProjectController:
+#     def __init__(self):
+#         self.should_run = True
+#
+#     def run(self):
+#
+#         Server = ServerManager()
+#         Endpoint = EndpointManager() 
+#
+#         while self.should_run:
+#             state = Server.server_state  
+#
+#             print("server in running")
+#             Server.process_cycle(state)
+#             Endpoint.process_cycle(state)
+#             break
+
+class ProjectController:
+    def __init__(self):
+        self.should_run = True
+
+    def run(self):
+        Server = ServerManager()
+        Endpoint = EndpointManager()
+
+        endpoint_thread = Thread(target=Endpoint.process_cycle, args=(Endpoint.endpoint_state,))
+        # server_thread = Thread(target=Server.process_cycle, args=(Server.server_state,))
+
+        endpoint_thread.start()
+        # server_thread.start()
+
+        endpoint_thread.join()
+        # server_thread.join()
+
+
+if __name__ == "__main__":
+    runner = ProjectController()
+    runner.run()
