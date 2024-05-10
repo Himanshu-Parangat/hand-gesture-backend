85f86b5debd1a20014ca25d94a134d70ea6a8c74
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-05-10 22:51:22 +0530

    simplifing websoket

--- src/websockets_handlers.py
+++ src/websockets_handlers.py
@@ -1,51 +1,19 @@
-from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
+import asyncio
+import websockets
+import datetime
 
-app = FastAPI()
+state = {"ws_state": "run"}
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
-
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+async def time_server(websocket):
     while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        if state["ws_state"] == "run":
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            await websocket.send(current_time)
+            await asyncio.sleep(0.2)
+        else:
+            break
+
+start_server = websockets.serve(time_server, "localhost", 8001)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
